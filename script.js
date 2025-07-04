b7f682f5d6d7070a74eb012b978295a75a9cdf40
pietro123445 <pietro.dias.ribeiro@escola.pr.gov.br>
2025-07-04 11:46:26 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+let mostraResultado = document.querySelector("#resultado");
+function cadastro() {
+   
+    let time = prompt("Digite o nome da equipe").toUpperCase();
+    let jog1 = prompt("Digite o nome do jogador 1");
+    let idade1 = Number(window.prompt('Digite a idade do jogador 1:'));
+    let jog2 = prompt("Digite o nome do jogador 2");
+    let idade2 = Number(window.prompt('Digite a idade do jogador 2:'));
+    let jog3 = prompt("Digite o nome do jogador 3");
+    let idade3 = Number(window.prompt('Digite a idade do jogador 3:'));
+    let times = 0;
+   
+
+   
+   
+     if(times <=4) {
+
+        times += 1;
+        mostraResultado.innerHTML +=`<p>time ${times}<p> Número de times: ${time}</p><p>Jogador: ${jog1}. Idade: ${idade1}</p><p>Jogador: ${jog2}. Idade: ${idade2}</p><p>Jogador: ${jog3}. Idade: ${idade3}</p>`;
+     
+    }else {
+            alert ("ja atingiu o numero maximo de times")
+        }
+       
+   
+   
+
+    }
